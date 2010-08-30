ed09eaaaf3daa3b84c2bd49879940d7f42c6afad
jbbarth <jeanbaptiste.barth@gmail.com>
2010-08-30 09:26:19 +0200

    Initial commit: just ran 'ruby script/generate redmine_plugin auto_assigned_status' ; it has generated these files in vendor/plugins/redmine_auto_assigned_status/ directory

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_auto_assigned_status do
+  name 'Redmine Auto Assigned Status plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
